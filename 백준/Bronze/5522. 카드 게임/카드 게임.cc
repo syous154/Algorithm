aa8c90ea39f5b5f4ff9e625e4aceb462dd16a9ed
Jae Hun Lee <56266206+syous154@users.noreply.github.com>
2024-03-15 15:27:21 +0900

    [Bronze V] Title: 카드 게임, Time: 0 ms, Memory: 2020 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Bronze/5522. 카드 게임/카드 게임.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int sum = 0;
+    int score;
+    
+    for(int i = 0 ; i < 5; i++){
+        cin>>score;
+        sum+=score;
+    }
+    cout<<sum<<endl;
+    return 0;
+}
