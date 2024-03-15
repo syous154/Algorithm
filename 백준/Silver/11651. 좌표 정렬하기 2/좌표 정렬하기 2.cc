b3a05c11f633fc45d879bdee600a7edd8fb68ed1
Jae Hun Lee <56266206+syous154@users.noreply.github.com>
2024-03-15 15:33:43 +0900

    [Silver V] Title: 좌표 정렬하기 2, Time: 120 ms, Memory: 2680 KB -BaekjoonHub

---
+++ 백준/Silver/11651. 좌표 정렬하기 2/좌표 정렬하기 2.cc
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+struct Point{
+    int x,y;
+};
+bool compare(Point now, Point last)
+{
+	if (now.y > last.y) return false;
+	if (now.y == last.y && now.x > last.x) return false;
+	else return true;
+}
+int main()
+{
+    struct Point p[100000];
+    int n;
+    
+    cin>>n;
+    
+    for(int i = 0 ; i < n ; i++){
+        cin>>p[i].x>>p[i].y;
+    }
+    sort(p,p+n,compare);
+    for(int i = 0 ; i <  n  ; i++){
+        cout<<p[i].x<<" "<<p[i].y<<'\n';
+    }
+    
+    return 0;
+}
