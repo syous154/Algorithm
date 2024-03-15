d56ecdd8c2d69951590533fcf9d0ff129035841a
Jae Hun Lee <56266206+syous154@users.noreply.github.com>
2024-03-15 15:24:24 +0900

    [Bronze III] Title: 곱셈, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2588. 곱셈/곱셈.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n1,n2;
+    cin>>n1>>n2;
+    
+    cout<<(n1*(n2%10))<<endl<<(n1 * ((n2-(n2/100)*100)/10))<<endl<<(n1*(n2/100))<<endl<<(n1*n2);
+    
+    return 0;
+}
