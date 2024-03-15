29e173d6469efcc53ec83dd2df548913b0158302
Jae Hun Lee <56266206+syous154@users.noreply.github.com>
2024-03-15 15:22:04 +0900

    [Bronze II] Title: 분해합, Time: 8 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2231. 분해합/분해합.cc
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    int num;
+    int sum,part;
+    cin>>num;
+    
+    for(int i = 1;i < num;i++){
+        sum = i;
+        part = i;
+        
+        while(part){
+            sum+=part%10;
+            part /= 10;
+        }
+        if(num == sum){
+            cout<<i<<endl;
+            return 0;
+        }
+    }
+    cout<<"0"<<endl;
+    return 0;
+}
