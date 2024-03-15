7ab09927414fe4b17faa6519afae243a1d55b12a
Jae Hun Lee <56266206+syous154@users.noreply.github.com>
2024-03-15 15:29:12 +0900

    [Silver III] Title: 돌 게임 3, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/9657. 돌 게임 3/돌 게임 3.cc
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int main(){
+
+  int n;
+  cin>>n;
+  bool arr[1001];
+  
+  arr[1] = 1;
+  arr[2] = 0;
+  arr[3] = 1;
+  arr[4] = 1;
+
+  for(int i=5; i<=n; ++i) {
+    if(arr[i-1] && arr[i-3] && arr[i-4]) arr[i] = 0;
+    else arr[i] = 1;
+  }
+
+  if(arr[n]) cout<<"SK";
+  else cout<<"CY";
+}
