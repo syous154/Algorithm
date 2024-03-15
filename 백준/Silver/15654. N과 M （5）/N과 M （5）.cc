9c96d45ab1880a4b303123f480446c7cdd3b6298
Jae Hun Lee <56266206+syous154@users.noreply.github.com>
2024-03-15 15:35:58 +0900

    [Silver III] Title: N과 M (5), Time: 44 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/15654. N과 M （5）/N과 M （5）.cc
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int n, m;
+int arr[8];
+bool check[8];
+int rst[8];
+void BT(int start, int depth) {
+	if (m == depth) {
+		for (int i = 0; i < m; i++) {
+			cout << rst[i] << ' ';
+		}
+		cout << '\n';
+
+		return;
+	}
+	for (int i = 0; i < n; i++) {
+		if (check[i]) {
+			continue;
+		}
+		check[i] = true;
+		rst[depth] = arr[i];
+		BT(i + 1, depth+1);
+		check[i] = false;
+	}
+}
+int main() {
+	cin >> n >> m;
+	
+	for (int i = 0; i < n; i++) {
+		cin >> arr[i];
+	}
+	sort(arr,arr + n);
+
+	BT(0, 0);
+}
