2169bb81424e33ce3fa1c0d55407530935ef8998
Jae Hun Lee <56266206+syous154@users.noreply.github.com>
2024-03-15 15:26:10 +0900

    [Bronze V] Title: 킹, 퀸, 룩, 비숍, 나이트, 폰, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/3003. 킹， 퀸， 룩， 비숍， 나이트， 폰/킹， 퀸， 룩， 비숍， 나이트， 폰.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+ 
+int main(){
+	
+    int a[6] = { 1, 1, 2, 2, 2, 8 };
+	int b[6];
+ 
+	for (int i = 0; i < 6; ++i) {
+		cin >> b[i];
+		cout << (b[i] - a[i]) * -1 << " ";
+	}
+	cout << '\n';
+ 
+	return 0;
+}
