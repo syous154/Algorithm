499d428fac3d39991746993a1c789dba2082ce46
Jae Hun Lee <56266206+syous154@users.noreply.github.com>
2024-03-15 15:29:06 +0900

    [Silver V] Title: 돌 게임 2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/9656. 돌 게임 2/돌 게임 2.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    if (n % 2 == 0) {
+        cout << "SK" << '\n';
+    }
+    else {
+        cout << "CY" << '\n';
+    }
+    return 0;
+}
