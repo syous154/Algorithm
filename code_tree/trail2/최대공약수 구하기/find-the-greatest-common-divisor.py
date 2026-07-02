dd7ec3e97f0c6e73a831642b465cbf7fee1f5100
Lee JH <56266206+syous154@users.noreply.github.com>
2026-07-02 17:59:16 +0900

    [Passed] 최대공약수 구하기 (50ms, 16MB)

---
+++ code_tree/trail2/최대공약수 구하기/find-the-greatest-common-divisor.py
@@ -0,0 +1,9 @@
+n, m = map(int, input().split())
+
+# Please write your code here.
+result = 0
+for i in range(1, min(n, m)  + 1):
+    if n % i == 0 and m % i == 0:
+        result = i
+
+print(result)
